734946de979c594167185a9907c6b9d202cf89ca
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2024-07-04 12:51:05 +0545

    propertyIniterance

---
+++ Father.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace inheritance
+{
+    public class Father
+    {
+        public string FatherName;
+        public string ParentalProperty;
+        public string age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inheritance
+{ 
+    public class Inheritance
+    {
+    }
+  
+
+
+
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Child c = new Child();
+            c.ParentalProperty = "Cheyya aapan ko paisay aabbu ka";
+            Console.WriteLine(c.ParentalProperty);
+            Console.ReadKey();
+        }
+    }
+}
